57619810d47754b4ca56a37f534130cf167845a3
lcoelho3412 <leandrocoelho3@yahoo.com>
2023-04-20 08:28:52 -0300

    feat: :fire: test

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,14 @@
+from inventory_report.inventory.inventory import Inventory
+import sys
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        return sys.stderr.write("Verifique os argumentos\n")
+    try:
+        path = sys.argv[1]
+        report_type_method = sys.argv[2]
+        report = Inventory.import_data(path, report_type_method)
+        sys.stdout.write(report)
+    except (ValueError):
+        sys.stdout.write("Verifique seus argumentos\n")
