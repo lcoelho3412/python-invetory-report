10041e7a01a386d2e0bca624c5e566218662d24c
lcoelho3412 <leandrocoelho3@yahoo.com>
2023-04-20 02:05:21 -0300

    feat: :bug: fix test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,21 +1,31 @@
 from inventory_report.inventory.product import Product
 
 
-def test_create_product():
-    product = Product(
-        id=95432,
-        nome_do_produto="Produto ficticio",
-        nome_da_empresa="Empresa ficticia",
-        data_de_fabricacao="10/08/2019",
-        data_de_validade="25/08/2024",
-        numero_de_serie="nsf021585",
-        instrucoes_de_armazenamento="local seco e arejado"
+def test_relatorio_produto():
+    product = {
+        "id": 95432,
+        "nome_do_produto": "Produto ficticio",
+        "nome_da_empresa": "Empresa ficticia",
+        "data_de_fabricacao": "10/08/2019",
+        "data_de_validade": "25/08/2024",
+        "numero_de_serie": "nsf021585",
+        "instrucoes_de_armazenamento": "local seco e arejado",
+    }
+
+    produto_ficticio = Product(
+        product["id"],
+        product["nome_do_produto"],
+        product["nome_da_empresa"],
+        product["data_de_fabricacao"],
+        product["data_de_validade"],
+        product["numero_de_serie"],
+        product["instrucoes_de_armazenamento"],
     )
 
-    assert product.id == 95432
-    assert product.nome_do_produto == "Produto ficticio"
-    assert product.nome_da_empresa == "Empresa ficticia"
-    assert product.data_de_fabricacao == "10/08/2019"
-    assert product.data_de_validade == "25/08/2024"
-    assert product.numero_de_serie == "nsf021585"
-    assert product.instrucoes_de_armazenamento == "local seco e arejado"
+    expected = (
+        "O produto Produto ficticio fabricado em 10/08/2019"
+        + " por Empresa ficticia com validade até 25/08/2024"
+        + " precisa ser armazenado local seco e arejado."
+    )
+
+    assert produto_ficticio.__repr__() == expected
